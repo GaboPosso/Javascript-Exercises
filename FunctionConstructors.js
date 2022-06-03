fb5e6d1c13835b862a6cf31954ab6e7a85c4fb26
GaboPosso <gabopos@gmail.com>
2022-06-03 11:41:53 -0500

    Add Function Constructors lecture and examples

--- FunctionConstructors.js
+++ FunctionConstructors.js
@@ -52,3 +52,73 @@ emily.getName()
 
 //Add  functions in constructor's prototype, it's a good practice. As the replicated methods on each
 // takes a lot of space. Instead, if we add the functions in the proto, everything shares the same.
+
+//Object.create
+
+//Creates a new object, using an existing object as the prototype of the new created object.
+
+var person3 = {
+    firstName3: 'default',
+    lastName3: 'default',
+};
+
+var Mathew = Object.create(person3);
+console.log(Mathew)
+console.log(Mathew.__proto__)
+
+//Thus, it can access to all the base object's properties an methods 
+//Now, if we wat to change some properties, the only thing we must do is to write
+//new properties with the same name of the object.
+Mathew.firstName3 = "Mathew";
+Mathew.lastName3 = "Stevenson";
+
+/*This is possible,thanks to the Prototype Chain, that searches that property first in the object,
+and if it finds it, there is no need to search it in the prototype.
+*/
+
+//#ES& and Classes
+
+/* In the last version of JS standard, JS has the possibilities to declare Classes,
+this is the concept used by other languages to implement inheritance. In any case, in JS 
+classes won't be the same as other languages, as Js has the prototypal model.
+*/
+
+//Let's watch how the classes are seen in Js
+
+class Person4 {
+    constructor (firstName, lastName){
+        this.firstName = firstName,
+        this.lastName =lastName
+    }
+    greet(){
+        console.log('Hi '+ this.firstName);
+    }
+}
+
+var gabo = new Person4('Gabriel','Mejia');
+gabo.greet();
+
+/* When a class is defined we use the new statement class. Inside it, first we define
+its constructor, which is the method that receives all the parameters 
+and will create the new objects based in the class. 
+
+To add a prototype to the created class use 'extend' keyword, which is also inspired by 
+other languages:
+*/
+class Employee extends Person4 { //Use Person4 as prototype for Employee
+    constructor (firstName, lastName, employment, salary){//receives all parameters
+        super(firstName, lastName);//Allow to use the class's constructor from which we're inheriting (Person4)
+        this.employment = employment;//Define own class parameters
+        this.salary = salary;
+    }
+}
+var Anna = new Employee('Anna', 'Garcia', 'Professor', 1000); //As we see, we can define Employee and Person4
+Anna.greet()
+
+//OBJECT ORIENTED PROGRAMMING
+/* A programming paradigm represents a particular approach or philosophy for designing solutions. The paradigms
+  differ from each other, in the concepts and the way of abstracting the elements involved in a problem, as well
+  as in the steps that integrate their solution of the problem, in other words, the computation. 
+  OOP (Object Oriented Programming) is a programming paradigm. where the data is encapsulated in objects that 
+  have properties and methods, and all the operations that we do are done on these objects.
+*/
